ac09e19a06d7eba9b087279e2b81b515deb803a4
Ayyub Abdurrahman <gamingayyub3@gmail.com>
2024-10-15 16:33:50 +0700

    p

--- test/api.test.js
+++ test/api.test.js
@@ -28,46 +28,5 @@ describe('API tesing', () => {
 
     });
 
-    it('should delete an item', (done) => {
-        const itemIdToDelete = {name: 'Item 1'};
-        request(app)
-            .delete(`/api/items/${itemIdToDelete}`)
-            .end((err, res) => {
-                expect(res.status).to.equal(200);
-                expect(res.body).to.have.property('message', 'Item deleted successfully');
-
-                request(app)
-                    .get('/api/items')
-                    .end((err, getRes) => {
-                        expect(getRes.status).to.equal(200);
-                        expect(getRes.body).to.be.an('array');
-                        const deletedItem = getRes.body.find(item => item.id === itemIdToDelete);
-                        expect(deletedItem).to.be.undefined;
-                        done();
-                    });
-            });
-    });
-
-    it('should update an item', (done) => {
-        const itemIdToUpdate = 2;
-        const updatedData = { name: 'Updated Item 2' };
-        request(app)
-            .put(`/api/items/${itemIdToUpdate}`)
-            .send(updatedData)
-            .end((err, res) => {
-                expect(res.status).to.equal(200);
-                expect(res.body).to.have.property('id', itemIdToUpdate);
-                expect(res.body).to.have.property('name', 'Updated Item 2');
-                request(app)
-                    .get('/api/items')
-                    .end((err, getRes) => {
-                        expect(getRes.status).to.equal(200);
-                        expect(getRes.body).to.be.an('array');
-                        const updatedItem = getRes.body.find(item => item.id === itemIdToUpdate);
-                        expect(updatedItem).to.not.be.undefined;
-                        expect(updatedItem.name).to.equal('Updated Item 2');
-                        done();
-                    });
-            });
-    });
+    
 });
